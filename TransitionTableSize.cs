4ae74684af7c8dd6b244772a2287f80c4d884c0e
Rida Salman <ridasalman458@gmail.com>
2022-11-22 14:52:07 +0500

    Create TransitionTableSize.cs

---
+++ TransitionTableSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_Project
+{
+    public static class TransitionTableSize
+    {
+        public static int[][] transitiontablearray(int rows,int colums)
+        {
+            int[][] array = new int[rows][];
+            for(int i = 0;i < rows; i++)
+            {
+                array[i] = new int[colums];
+            }
+
+            return array;
+        }
+    }
+}
