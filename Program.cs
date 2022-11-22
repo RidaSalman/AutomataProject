d48fe9cfaa39e5e89daba55cefe1ee9659364dd8
Rida Salman <ridasalman458@gmail.com>
2022-11-22 14:52:03 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Text : ");
+            char[] text = Console.ReadLine().ToCharArray();
+
+            Console.WriteLine("Enter Keyword : ");
+            char[] keyword = Console.ReadLine().ToCharArray();
+
+            Logic.findKeywords(keyword, text);
+           
+            Console.ReadLine();
+
+        }
+    }
+}
